b05101fbfccb7b028617a8e6352675a9196f5d82
Christof Bless <14159998+Christof93@users.noreply.github.com>
2024-06-14 17:00:06 +0200

    build environment

--- simulation/reputation-environment/env/reputation_environment.py
+++ simulation/reputation-environment/env/reputation_environment.py
@@ -3,12 +3,12 @@ import random
 from copy import copy
 
 import numpy as np
-from gymnasium.spaces import Discrete, MultiDiscrete
+from gymnasium.spaces import Discrete, Box, Dict, MultiDiscrete
 
 from pettingzoo import ParallelEnv
 
 
-class CustomActionMaskedEnvironment(ParallelEnv):
+class ReputationEnvironment(ParallelEnv):
     """The metadata holds environment constants.
 
     The "name" metadata allows the environment to be pretty printed.
@@ -18,7 +18,7 @@ class CustomActionMaskedEnvironment(ParallelEnv):
         "name": "reputation_environment_v0",
     }
 
-    def __init__(self, n_authors=1, n_conferences=1):
+    def __init__(self, n_authors=2, n_conferences=1):
         """The init method takes in environment arguments.
 
         Should define the following attributes:
@@ -37,7 +37,74 @@ class CustomActionMaskedEnvironment(ParallelEnv):
         self.timestep = None
         self.n_authors = n_authors
         self.n_conferences = n_conferences
-        self.possible_agents = ["author", "conference"]
+        self.possible_agents = []
+        self.agent_to_id = {}
+        self.max_papers = 5
+        self.max_authors = self.n_authors
+        self.observations = {}
+        self.action_masks = {}
+        self.rewards = {}
+        self.conferences = np.array((self.n_conferences,), dtype=np.int32)
+        for i in range(self.n_authors):
+            agent = f"author_{i}"
+            self.possible_agents.append(agent)
+            self.agent_to_id[agent] = i
+            
+    def _get_action_mask(self, agent, observation=None):
+        action_mask = None
+        previous_mask = self.action_masks[agent]
+        # construct new mask based on observed state of environment
+        action_mask = previous_mask
+        
+        return action_mask
+    
+    def _observe_after_action(self, agent, action=None):
+        observation = self.observations[agent]
+        reward = 0
+        # make a new observation after applying the changes induced by action
+        
+        ## check the submission action
+        submitted_paper = action["submit"]["id"]
+        submit_to = action["submit"]["conference"]
+
+        if submitted_paper > 0:
+            observation["papers"]["authors"][submitted_paper:,] = 0
+            effort = observation["papers"]["effort"][submitted_paper]
+            conference = self.conferences[submit_to]
+            reward = self._reward_paper(effort, conference)
+        
+        ## check the new paper action
+        if action["start_with_coauthors"] > 0:
+            started = False
+            for i, (assigned, wanted, _) in enumerate(observation["papers"]["authors"]):
+                ## other author(s) looking for same amount of coauthors
+                if assigned < wanted and wanted == action["start_with_coauthors"]:
+                    observation["papers"]["authors"][i, 0] += 1  
+                    started = True
+                    print("assigned to paper: ", observation)
+                    break
+            if not started:
+                for i, (assigned, wanted, _) in enumerate(observation["papers"]["authors"]):
+                ## other author(s) looking for same amount of coauthors
+                    if wanted == 0:
+                        observation["papers"]["authors"][i, 1] = action["start_with_coauthors"]  
+                        started = True
+                        print("started new paper: ", observation)
+                        break
+        
+        ## contribute action
+        observation["papers"]["effort"][action["contribute"]] += 1
+
+        return observation, reward
+
+    def _reward_paper(self, effort, conference):
+        potential_reward = self.conferences[conference]/10
+        ## accept?
+        if sigmoid(effort - potential_reward) + np.random.normal(0, 0.05) > 0.5:
+            return potential_reward
+        else:
+            return 0
+    
 
     def reset(self, seed=None, options=None):
         """Reset set the environment to a starting point.
@@ -45,30 +112,43 @@ class CustomActionMaskedEnvironment(ParallelEnv):
         It needs to initialize the following attributes:
         - agents
         - timestamp
-        - prisoner x and y coordinates
-        - guard x and y coordinates
-        - escape x and y coordinates
         - observation
         - infos
 
         And must set up the environment so that render(), step(), and observe() can be called without issues.
         """
-        self.agents = [self.possible_agents[0]] * self.n_authors + [self.possible_agents[1]] * self.n_conferences
+        self.agents = copy(self.possible_agents)
         self.timestep = 0
+        self.conferences = np.floor(np.random.normal(50, 25, self.n_conferences))
+        self.reputations = np.full((self.n_authors,),20, dtype=np.int32)
+        observations = {}
+        for agent in self.agents:
+            print(self.action_space(agent))
+            self.observations[agent] = {
+                'spendable_tokens': self.conferences,
+                'agent_reputation': self.reputations[self.agent_to_id[agent]],
+                'papers': {
+                    "effort": np.zeros((self.max_papers,), dtype=np.int32),
+                    "authors": np.zeros((self.max_papers * self.n_authors, 3), dtype=np.int32),
+                }
+            }
+            self.action_masks[agent] = {
+                "start_with_coauthors": np.ones((self.max_authors + 1,), dtype=np.int8), # start a paper,
+                "submit": { # submit a paper,
+                    "id": np.zeros((self.n_authors * self.max_papers,), dtype=np.int8),
+                    "conference": np.zeros((self.n_conferences + 1,), dtype=np.int8),
+                },
+                "contribute": np.zeros((self.n_authors * self.max_papers,), dtype=np.int8) # contribute to paper
+            }
 
-        observation = (
-            self.prisoner_x + 7 * self.prisoner_y,
-            self.guard_x + 7 * self.guard_y,
-            self.escape_x + 7 * self.escape_y,
-        )
-        observations = {
-            "prisoner": {"observation": observation, "action_mask": [0, 1, 1, 0]},
-            "guard": {"observation": observation, "action_mask": [1, 0, 0, 1]},
-        }
+            observations[agent] = {
+                "observation": self.observations[agent], 
+                "action_mask": self.action_masks[agent],
+            }
 
         # Get dummy infos. Necessary for proper parallel_to_aec conversion
         infos = {a: {} for a in self.agents}
-
+        print(observations)
         return observations, infos
 
     def step(self, actions):
@@ -86,105 +166,34 @@ class CustomActionMaskedEnvironment(ParallelEnv):
         And any internal state used by observe() or render()
         """
         # Execute actions
-        prisoner_action = actions["prisoner"]
-        guard_action = actions["guard"]
 
-        if prisoner_action == 0 and self.prisoner_x > 0:
-            self.prisoner_x -= 1
-        elif prisoner_action == 1 and self.prisoner_x < 6:
-            self.prisoner_x += 1
-        elif prisoner_action == 2 and self.prisoner_y > 0:
-            self.prisoner_y -= 1
-        elif prisoner_action == 3 and self.prisoner_y < 6:
-            self.prisoner_y += 1
-
-        if guard_action == 0 and self.guard_x > 0:
-            self.guard_x -= 1
-        elif guard_action == 1 and self.guard_x < 6:
-            self.guard_x += 1
-        elif guard_action == 2 and self.guard_y > 0:
-            self.guard_y -= 1
-        elif guard_action == 3 and self.guard_y < 6:
-            self.guard_y += 1
-
-        # Generate action masks
-        prisoner_action_mask = np.ones(4, dtype=np.int8)
-        if self.prisoner_x == 0:
-            prisoner_action_mask[0] = 0  # Block left movement
-        elif self.prisoner_x == 6:
-            prisoner_action_mask[1] = 0  # Block right movement
-        if self.prisoner_y == 0:
-            prisoner_action_mask[2] = 0  # Block down movement
-        elif self.prisoner_y == 6:
-            prisoner_action_mask[3] = 0  # Block up movement
-
-        guard_action_mask = np.ones(4, dtype=np.int8)
-        if self.guard_x == 0:
-            guard_action_mask[0] = 0
-        elif self.guard_x == 6:
-            guard_action_mask[1] = 0
-        if self.guard_y == 0:
-            guard_action_mask[2] = 0
-        elif self.guard_y == 6:
-            guard_action_mask[3] = 0
-
-        # Action mask to prevent guard from going over escape cell
-        if self.guard_x - 1 == self.escape_x:
-            guard_action_mask[0] = 0
-        elif self.guard_x + 1 == self.escape_x:
-            guard_action_mask[1] = 0
-        if self.guard_y - 1 == self.escape_y:
-            guard_action_mask[2] = 0
-        elif self.guard_y + 1 == self.escape_y:
-            guard_action_mask[3] = 0
-
+        observations = {}
+        # Get dummy infos (not used in this example)
+        infos = {}
         # Check termination conditions
         terminations = {a: False for a in self.agents}
-        rewards = {a: 0 for a in self.agents}
-        if self.prisoner_x == self.guard_x and self.prisoner_y == self.guard_y:
-            rewards = {"prisoner": -1, "guard": 1}
-            terminations = {a: True for a in self.agents}
-            self.agents = []
-
-        elif self.prisoner_x == self.escape_x and self.prisoner_y == self.escape_y:
-            rewards = {"prisoner": 1, "guard": -1}
-            terminations = {a: True for a in self.agents}
-            self.agents = []
-
-        # Check truncation conditions (overwrites termination conditions)
-        truncations = {"prisoner": False, "guard": False}
-        if self.timestep > 100:
-            rewards = {"prisoner": 0, "guard": 0}
-            truncations = {"prisoner": True, "guard": True}
-            self.agents = []
-        self.timestep += 1
-
-        # Get observations
-        observation = (
-            self.prisoner_x + 7 * self.prisoner_y,
-            self.guard_x + 7 * self.guard_y,
-            self.escape_x + 7 * self.escape_y,
-        )
-        observations = {
-            "prisoner": {
+        truncations = {a: False for a in self.agents}
+        rewards = {}
+        for agent, action in actions.items():
+            print(agent)
+            print(action)
+            # Check truncation conditions (overwrites termination conditions)
+            
+            # Get observations
+            rewards[agent], observation = self._observe_after_action(agent, action)
+            # Generate action masks
+            action_mask = self._get_action_mask(agent, observation)
+            observations[agent] = {
                 "observation": observation,
-                "action_mask": prisoner_action_mask,
-            },
-            "guard": {"observation": observation, "action_mask": guard_action_mask},
-        }
-
-        # Get dummy infos (not used in this example)
-        infos = {"prisoner": {}, "guard": {}}
+                "action_mask": action_mask,
+            }
+            infos[agent] = {}
+            self.timestep += 1
 
         return observations, rewards, terminations, truncations, infos
 
     def render(self):
         """Renders the environment."""
-        grid = np.zeros((7, 7))
-        grid[self.prisoner_y, self.prisoner_x] = "P"
-        grid[self.guard_y, self.guard_x] = "G"
-        grid[self.escape_y, self.escape_x] = "E"
-        print(f"{grid} \n")
 
     # Observation space should be defined here.
     # lru_cache allows observation and action spaces to be memoized, reducing clock cycles required to get each agent's space.
@@ -192,10 +201,43 @@ class CustomActionMaskedEnvironment(ParallelEnv):
     @functools.lru_cache(maxsize=None)
     def observation_space(self, agent):
         # gymnasium spaces are defined and documented here: https://gymnasium.farama.org/api/spaces/
-        return MultiDiscrete([7 * 7 - 1] * 3)
+        return Dict({
+            'spendable_tokens': Box(
+                low=0,
+                high=1_000_000,
+                shape=(self.n_conferences,),
+                dtype=np.int32
+            ),
+            'agent_reputation': Discrete(1000),  # Example max reputation tokens
+            'papers': {
+                "effort": Box(
+                    low=0,
+                    high=12,
+                    shape=(self.n_authors * self.max_papers, ),
+                    dtype=np.int32
+                ),
+                "authors": Box(
+                    low=0, 
+                    high=10, 
+                    shape=(self.max_papers * self.n_authors, 3), # assigned authors, wanted authors, finished authors
+                    dtype=np.int32
+                ),
+            }
+        })
 
     # Action space should be defined here.
     # If your spaces change over time, remove this line (disable caching).
     @functools.lru_cache(maxsize=None)
     def action_space(self, agent):
-        return Discrete(4)
+        return Dict({
+            "start_with_coauthors": Discrete(self.max_authors + 1), # start a paper,
+            "submit": Dict({ # submit a paper,
+                "id": Discrete(self.n_authors * self.max_papers),
+                "conference": Discrete(self.n_conferences + 1),
+            }),
+            "contribute": Discrete(self.n_authors * self.max_papers) # contribute to paper
+        })
+    
+
+def sigmoid(x, alpha=1, x_shift = 10):
+    return 1 / (1 + np.exp(-alpha * x + x_shift))
--- simulation/reputation-environment/reputation_environment_v0.py
+++ simulation/reputation-environment/reputation_environment_v0.py
@@ -2,12 +2,12 @@ from env.reputation_environment import ReputationEnvironment
 
 env = ReputationEnvironment()
 observations, infos = env.reset()
-print(env.agents)
 while env.agents:
     # this is where you would insert your policy
-    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+    actions = {agent: env.action_space(agent).sample(mask = env.action_masks[agent]) for agent in env.agents}
 
     observations, rewards, terminations, truncations, infos = env.step(actions)
+    breakpoint()
 env.render()
 print(rewards)
 env.close()
